983bbca4677857a7a3b8e91985e4e2a297e6af2b
Richard <richardchekwas@gmail.com>
2023-11-10 18:05:29 +0100

    aa

--- web_flask/static/scripts/chekwasy_farm_script.js
+++ web_flask/static/scripts/chekwasy_farm_script.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
     $( '#chekwasy_farm_form' ).on( "submit", function( event ) {
 	const api = 'http://' + window.location.hostname;
 	$.ajax({
-	    url: api + ':5001/api/v1/chekwasy_farm/order',
+	    url: 'chekwasy.tech/api/v1/chekwasy_farm/order',
 	    type: 'POST',
-	    data: {"first_name": $('first_name').val(), "last_name": $('last_name').val(), "phone": $('phone').val(), "email": $('email').val(), "reference": $('reference').val(), "state": $('state').val(), "city": $('city').val(), "street": $('street').val(), "order_qty": $('order_qty').val() },
-	    contentType: 'application/json',
-	    dataType: 'json'
+	    data: {"first_name": $('#first_name').val(), "last_name": $('#last_name').val(), "phone": $('#phone').val(), "email": $('#email').val(), "reference": $('#reference').val(), "state": $('#state').val(), "city": $('#city').val(), "street": $('#street').val(), "#order_qty": $('#order_qty').val() }
 	})
 
 	    .done(function(data) {
