26dbf731d9532b572cee9110983f265d05f17879
Richard Chekwas <richardchekwas@chekwasy.myguest.virtualbox.org>
2024-04-01 11:03:53 +0100

    4th contact assignment

--- web_flask/static/scripts/farm_script.js
+++ web_flask/static/scripts/farm_script.js
@@ -10,19 +10,19 @@ const ls = [
     ];
 
 if (ls.includes(window.location.pathname)) {
+    const sess = document.getElementById("farm_session_id").innerHTML
 	const api = 'http://' + "chekwasy.tech";
-        $.ajax({
-               url: api + '/api/v2/farm_profile',
-                    type: 'GET',
-                    data: JSON.stringify({}),
-                    contentType: 'application/json',
-                    dataType: 'json'
-                })
+    $.ajax({
+        url: api + '/api/v2/farm_check/' + sess,
+        type: 'GET',
+        data: JSON.stringify({}),
+        contentType: 'application/json',
+        dataType: 'json'
+    })
 
-                    .done(function(data) {
-                                document.getElementById("f_name").innerHTML = `>
-                                document.getElementById("Email").innerHTML = `E>
-                                document.getElementById("Phone").innerHTML = `P>
-                                document.getElementById("Address").innerHTML = >
-                    });
-        };
+    .done(function(data) {
+    })
+    .fail(function() {
+        window.location.href = "/farm_login";
+    });
+};
