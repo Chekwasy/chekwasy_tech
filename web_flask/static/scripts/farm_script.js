afc5b9003c6b6fa07e60ec4ed66ffc311641acfb
Richard Chukwuchekwa <richardchekwas@gmail.com>
2024-04-01 11:23:26 +0000

    QQ

--- web_flask/static/scripts/farm_script.js
+++ web_flask/static/scripts/farm_script.js
@@ -8,12 +8,24 @@ const ls = [
         '/farm_update/',
         '/farm_update',
     ];
+function getCookieValue(cookieName) {
+    var name = cookieName + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
 
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i].trim();
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return "";
+}
 if (ls.includes(window.location.pathname)) {
-    const sess = document.getElementById("farm_session_id").innerHTML
-	const api = 'http://' + "chekwasy.tech";
+    var cooki = getCookieValue("farm_session_id");
+    const api = 'http://' + "chekwasy.tech"
     $.ajax({
-        url: api + '/api/v2/farm_check/' + sess,
+        url: api + '/api/v2/farm_check/' + cooki,
         type: 'GET',
         data: JSON.stringify({}),
         contentType: 'application/json',
